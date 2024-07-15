3da8563e82047cd61e6ecada5ce519c5946c034a
Kira Fox <136164233+kirafoxy@users.noreply.github.com>
2024-07-15 13:16:43 +0300

    Update magic.js

--- magic.js
+++ magic.js
@@ -16,64 +16,65 @@ function getUserInput() {
     console.log(numberOfQuestion);
 
     let firstOfSum = numberOfQuestion % 10;
-    //14%10=4
+    
     console.log(firstOfSum);
     
-    let secondOfNum = numberOfQuestion - firstOfSum;
+    let secondOfNum = (numberOfQuestion - firstOfSum)/ 10;
     console.log(secondOfNum);
 
-
-    let answerNum = firstOfSum + secondOfNum;
+    let answerNum = Number(firstOfSum) + Number(secondOfNum);
 
    console.log(answerNum);
-    // let answerForQuestion = firstOfSum + secondOfNum;
-    // console.log(answerForQuestion);
+   
+    switch(answerNum) {
+        case 1:
+        case 10:
+        case 19:
+            alert('Ты знаешь сам(а)')
+            break;
 
+        case 2:
+        case 11:
+            alert('Да!')
+            break;
+            
+        case 3:
+        case 12:
+            alert('Нет!')
+            break;
 
+        case 4:
+        case 13:
+            alert('Обязательно!')
+            break;
+
+        case 5:
+        case 14:
+            alert('Возможно.')
+            break; 
+            
+        case 6:
+        case 15:
+            alert('Будут препятсвия.')
+            break;
+
+        case 7:
+        case 16:
+            alert('Надейся и жди...')
+            break;
+
+        case 8:
+        case 17:
+            alert('Да, но не сейчас.')
+            break;
+            
+        case 9:
+        case 18:
+            alert('Позже.')
+            break;
+
+        default:
+            alert('Тут даже звезды не знают...')
+            break;
+    }
 }
-
-// let numberOfQuestion = userQuestion.length;
-// console.log(numberOfQuestion);
-
-//заменить на вывод текста в html
-// switch(answer) {
-//     case 1:
-//         alert('')
-//         break;
-
-//     case 2:
-//         alert('')
-//         break;
-        
-//     case 3:
-//         alert('')
-//         break;
-
-//     case 4:
-//         alert('')
-//         break;
-
-//     case 5:
-//         alert('')
-//         break; 
-        
-//     case 6:
-//         alert('')
-//         break;
-
-//     case 7:
-//         alert('')
-//         break;
-
-//     case 8:
-//         alert('')
-//         break;
-        
-//     case 9:
-//         alert('')
-//         break;
-
-//     default:
-//         alert('Тут даже звезды не знают...')
-//         break;
-// }
